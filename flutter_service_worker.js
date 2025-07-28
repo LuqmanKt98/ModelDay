f470db645e0e68e4911118aa94fc3bef9b3a3312
LuqmanKt98 <luqmankt98@gmail.com>
2025-07-29 02:29:30 +0500

    Clean rebuild without OAuth secrets
    - Performed flutter clean and fresh build
    - Removed all OAuth secrets from compiled JavaScript
    - Ready for secure deployment to GitHub and Vercel

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "76c981e709f0427dc3bc8104ce4b31f0
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-storage-cors.js": "81a76bf3ffa0f343420f6510c12b5521",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "b853e323e82bc3b3786ea1bea30857a6",
+"flutter_bootstrap.js": "436535a2f2415c2808d9cc5b9fc2b7c9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
