c2ecf51919ebfc096de065eff3cfae911f71fdb2
LuqmanKt98 <luqmankt98@gmail.com>
2025-07-29 00:45:21 +0500

    Deploy Flutter web build as static files for Vercel compatibility

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4043361535"
+    serviceWorkerVersion: "1348315036"
   }
 });
